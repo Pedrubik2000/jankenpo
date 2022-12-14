811029c597baa21ec777c44d6802368060993ac2
Pedrubik <119679067+Pedrubik2000@users.noreply.github.com>
2022-12-14 05:02:01 -0500

    Add game function

--- jankenpo.js
+++ jankenpo.js
@@ -1,40 +1,39 @@
 function getComputerChoice(){
     let choices = ["rock","paper","scissors"];
-    console.log(choices)
     let choice = choices[Math.floor(Math.random()*choices.length)];
-    console.log(choice);
     return(choice)
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection = "rock"){
-        if (computerSelection = "scissors"){
+    console.log(computerSelection);
+    if (playerSelection == "rock"){
+        if (computerSelection == "scissors"){
             return(`You win! ${playerSelection} beats ${computerSelection}`)
         }
-        else if (computerSelection = "paper"){
+        else if (computerSelection == "paper"){
             return(`You lose! ${playerSelection} beats ${computerSelection}`)
         }
         else {
             return("Draw!")
         }
     }
-    else if (playerSelection = "scissors"){
-        if (computerSelection = "paper"){
+    else if (playerSelection == "scissors"){
+        if (computerSelection == "paper"){
             return(`You win! ${playerSelection} beats ${computerSelection}`)
         }
-        else if (computerSelection = "rock"){
+        else if (computerSelection == "rock"){
             return(`You lose! ${playerSelection} beats ${computerSelection}`)
         }
         else {
             return("Draw!")
         }
     }
-    else if (playerSelection = "paper"){
-        if (computerSelection = "rock"){
+    else if (playerSelection == "paper"){
+        if (computerSelection == "rock"){
             return(`You win! ${playerSelection} beats ${computerSelection}`)
         }
-        else if (computerSelection = "scissors"){
+        else if (computerSelection == "scissors"){
             return(`You lose! ${playerSelection} beats ${computerSelection}`)
         }
         else {
@@ -42,9 +41,42 @@ function playRound(playerSelection, computerSelection) {
         }
     }
     else{
+
         return("Please input a valid option")
     }
-  }
+  };
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt()
+        computerSelection = getComputerChoice();
+        let gameResults = playRound(playerSelection, computerSelection);
+        console.log(gameResults)
+        console.log(i)
+        if (gameResults.includes("win")){
+            playerScore++
+        }
+        else if (gameResults.includes("lose")){
+            computerScore++
+        }
+        else if (gameResults.includes("Draw")){
+        }
+        else{
+            i--
+        };
+     };
+     if (playerScore > computerScore){
+        console.log(`${playerScore}-${computerScore}`)
+        console.log("Player wins!")
+     }
+     else if (playerScore < computerScore){
+        console.log(`${playerScore}-${computerScore}`)
+        console.log("Player lose!")
+     }
+     else{
+        console.log(`${playerScore}-${computerScore}`)
+        console.log("Draw!")
+     }
+};
