d16d54427f0f03962005d2ef131d004bd1be13ae
Pedrubik <119679067+Pedrubik2000@users.noreply.github.com>
2022-12-14 04:14:49 -0500

    Add playRound function

--- jankenpo.js
+++ jankenpo.js
@@ -1,5 +1,50 @@
 function getComputerChoice(){
-    let choices = ["Rock","Paper","Scissors"];
-    let choice = choices[Math.floor(Math.random*choice.length())];
+    let choices = ["rock","paper","scissors"];
+    console.log(choices)
+    let choice = choices[Math.floor(Math.random()*choices.length)];
     console.log(choice);
+    return(choice)
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection = "rock"){
+        if (computerSelection = "scissors"){
+            return(`You win! ${playerSelection} beats ${computerSelection}`)
+        }
+        else if (computerSelection = "paper"){
+            return(`You lose! ${playerSelection} beats ${computerSelection}`)
+        }
+        else {
+            return("Draw!")
+        }
+    }
+    else if (playerSelection = "scissors"){
+        if (computerSelection = "paper"){
+            return(`You win! ${playerSelection} beats ${computerSelection}`)
+        }
+        else if (computerSelection = "rock"){
+            return(`You lose! ${playerSelection} beats ${computerSelection}`)
+        }
+        else {
+            return("Draw!")
+        }
+    }
+    else if (playerSelection = "paper"){
+        if (computerSelection = "rock"){
+            return(`You win! ${playerSelection} beats ${computerSelection}`)
+        }
+        else if (computerSelection = "scissors"){
+            return(`You lose! ${playerSelection} beats ${computerSelection}`)
+        }
+        else {
+            return("Draw!")
+        }
+    }
+    else{
+        return("Please input a valid option")
+    }
+  }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
